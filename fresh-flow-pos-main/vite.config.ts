f79d8596a3dc1c020562ff46a5195bac7d2735c6
Crazyxyz45 <parthbiradar28@gmail.com>
2026-01-13 19:22:50 +0530

    Fix router path

--- fresh-flow-pos-main/vite.config.ts
+++ fresh-flow-pos-main/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/AgroVia-POS/",
+  base: "/AgroViaPos/",
   server: {
     host: "::",
     port: 8080,
